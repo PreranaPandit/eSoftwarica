5561e0b7fe7873c5d5b2710079f17763475b1bd3
PreranaPandit <preranapandit07@gmail.com>
2019-12-11 16:27:34 +0545

    RecyclerView Completion(first segment)

---
+++ app/src/main/java/com/example/esoftwarica/StudentsAdapter.java
@@ -0,0 +1,76 @@
+package com.example.esoftwarica;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class StudentsAdapter extends RecyclerView.Adapter<StudentsAdapter.StudentsViewHolder>{
+
+    Context mContext;
+    List<Students> studentsList;
+
+    //constructor, to receive data from activity
+
+
+    public StudentsAdapter(Context mContext, List<Students> studentsList) {
+        this.mContext = mContext;
+        this.studentsList = studentsList;
+    }
+
+    @NonNull
+    @Override
+    public StudentsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.student_details,parent,false);
+
+        return new StudentsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentsViewHolder holder, int position) {
+
+        Students students = studentsList.get(position);
+        holder.imgProfile.setImageResource(students.getImgProfile());
+        holder.tvName.setText(students.getName());
+        holder.tvAge.setText(String.valueOf(students.getAge()));
+        holder.tvAddress.setText(students.getAddress());
+        holder.tvGender.setText(students.getGender());
+        holder.imgBtnDel.setImageResource(students.getImgBtnDel());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentsList.size();
+    }
+
+    public class StudentsViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView imgProfile;
+        TextView tvName, tvAge, tvAddress, tvGender;
+        ImageButton imgBtnDel;
+
+
+        public StudentsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imgProfile = itemView.findViewById(R.id.imgProfile);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvAge = itemView.findViewById(R.id.tvAge);
+            tvAddress = itemView.findViewById(R.id.tvAddress);
+            tvGender = itemView.findViewById(R.id.tvGender);
+            imgBtnDel = itemView.findViewById(R.id.imgBtnDel);
+
+
+        }
+    }
+}
